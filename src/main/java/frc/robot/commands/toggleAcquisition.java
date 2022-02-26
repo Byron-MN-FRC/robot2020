beb7379aef540b93b054757182976449a6f77228
kmbrinkmann <kbrink29@students.byron.k12.mn.us>
2022-02-25 20:37:40 -0600

    Added acquisition solenoid

--- src/main/java/frc/robot/commands/toggleAcquisition.java
+++ src/main/java/frc/robot/commands/toggleAcquisition.java
@@ -49,12 +49,13 @@ public class toggleAcquisition extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_ballAcquisition.toggleAcquisition();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_ballAcquisition.toggleAcquisition();
+        
     }
 
     // Called once the command ends or is interrupted.
