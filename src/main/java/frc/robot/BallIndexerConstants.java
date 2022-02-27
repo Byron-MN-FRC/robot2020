c1cbb7329252364b75c86ae36f91e8a30cf2ab27
kmbrinkmann <kbrink29@students.byron.k12.mn.us>
2022-02-26 21:43:37 -0600

    ball indexer

---
+++ src/main/java/frc/robot/BallIndexerConstants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class BallIndexerConstants {
+	public static final double indexMotorSpeed = 0.7;
+	public static final double shootMotorSpeed = 1.0;
+	public static final double reverseMotorSpeed = -0.7;
+
+	public static final double indexOneTimeOut = 2.5;
+	public static final double indexToShootTimeout = 6.0;
+	public static final double indexReverseOneTimeout = 3.0;
+
+}
